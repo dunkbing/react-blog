28f94e9aff17c8045832ee91f1788e562e605cda
binh <dangbinh4869@gmail.com>
2021-03-06 16:00:13 +0700

    + setup my personal blog

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "YWZmNjVlOGExNDkwMDc2OTkxOGQwODI3YzI1OWQ4YjYyMDU4ZWFiYQ==",
+
+  // Your Github UserName
+  githubUserName: "dunkbing",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.9f40fd53257d4102',
+  OAuthSecret: 'fb6c64b25c3dd426763af53b84bc9b9cd3a081c1',
+
+  // Your Personal Blog Title
+  title : "Dunkbing" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
