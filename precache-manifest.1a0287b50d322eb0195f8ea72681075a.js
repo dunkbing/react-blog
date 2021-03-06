86807a0536f8611425bec85820d0cd0e0b37b6ff
binh <dangbinh4869@gmail.com>
2021-03-06 16:04:50 +0700

    Updates

---
+++ precache-manifest.1a0287b50d322eb0195f8ea72681075a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b118c6519ae4edf8fa80fc20f719b29",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "5beff6d5a627c58551aa",
+    "url": "/react-blog/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "f52afa6565eb1d99d8c4",
+    "url": "/react-blog/static/js/2.83b5ef32.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog/static/js/2.83b5ef32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5beff6d5a627c58551aa",
+    "url": "/react-blog/static/js/main.b2ea2249.chunk.js"
+  },
+  {
+    "revision": "c545bb6f24b0525a1b88",
+    "url": "/react-blog/static/js/runtime-main.635d1ba4.js"
+  }
+]);
